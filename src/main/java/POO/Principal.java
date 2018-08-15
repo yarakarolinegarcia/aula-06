cc61764d51c9f35dd1d54b82674e8ea2c7e9f668
Yara Garcia <yarakarolinegarcia@gmail.com>
2018-08-15 08:17:36 -0300

    Imprime meu nome

--- src/main/java/POO/Principal.java
+++ src/main/java/POO/Principal.java
@@ -3,7 +3,7 @@ package POO;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Olá mundo");
+        System.out.println("Yara Karoline Garcia");
 
     }
 }
